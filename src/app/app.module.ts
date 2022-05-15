04a317ab6db788233b592406a7a3e63b6f353df6
Leiner Castro <leinercg23@outlook.com>
2022-05-15 15:04:38 -0600

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WeatherModule } from './pages/weather/weather.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSelectModule } from '@angular/material/select';
+
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    WeatherModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    MatSelectModule,
+    NgxMatSelectSearchModule,
+    WeatherModule,
+ 
   ],
   providers: [],
   bootstrap: [AppComponent]
